85e3988f7010e830cf8ab56b7ec87db21b835af4
ArtTom <tomasiewicz.artur@gmail.com>
2020-01-08 23:28:52 +0100

    guessing..

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,48 @@
+function isAnumber () {
+	number='^[0-9]+$'
+    if ! [[ $response =~ $number ]]
+	then
+		echo 1
+		return 1
+	fi
+
+}
+
+function file_cnt {
+	local cnt=`ls -l . | egrep -c '^-'`
+    echo $cnt
+}
+
+correct=$(file_cnt)
+
+
+
 echo "How many files are in the current directory ?"
 echo "Enter your answer"
 read response
 
 
-function file_cnt {
-        local cnt=$(ls -l | wc -l)-1
-        echo $cnt
-}
 
-correct=$(file_cnt)
 
 while [[ $response -ne $correct ]]
 do
+	checkNumber=$( isAnumber $response)
+	if [[ $checkNumber -eq 0 ]]
+	then 
         if [[ $response -gt $correct ]]
         then
                 echo "No.. it's too high"
-        else
+        elif [[ $response -lt $correct ]]
+        then
                 echo "No.. it's to low"
         fi
-        echo
-        echo "Try again: "
+
+        echo "Try again:"
         read response
+	else
+		echo "Please type a number:" 
+		read response
+	fi
 done
 
 echo "Great! You guess how many files are in the current directory"
